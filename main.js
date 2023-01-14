e28cae6471bdc1d9a926e59e3a968419a29470f1
Derpy <bottledcanofdoom@gmail.com>
2023-01-14 14:13:24 -0700

    ice

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ var settings = {
     height:80,
     depth:160,
     tileSize:8,
-    smoothness:3,
+    smoothness:5,
     variation:10,
     offset:-1,
-    caveCount:10
+    caveCount:10,
+    meteors:3,
+    nitrogenDeposits:10
 }
 
 var c = document.getElementById("mainCanvas");
@@ -80,17 +82,29 @@ class astronaut{
 }
 
 var waterupdate = function(){
+    return;
+    var tempx = this.x;
+    var tempy = this.y;
+    var tempz = this.z;
     if(map[this.x][this.y][this.z+1].texture == null){
         var temp = map[this.x][this.y][this.z+1]
         map[this.x][this.y][this.z+1] = map[this.x][this.y][this.z]
         map[this.x][this.y][this.z] = temp;
+        map[tempx][tempy][tempz+1].z -= 1;
+        map[tempx][tempy][tempz].z += 1;
+        map[tempx][tempy][tempz].update = function(){return;};
+        map[tempx][tempy][tempz+1].update = waterupdate;
         return;
     }
     if(!oob(this.x+1,this.y)){
         if(map[this.x+1][this.y][this.z].texture == null){
             var temp = map[this.x+1][this.y][this.z]
             map[this.x+1][this.y][this.z] = map[this.x][this.y][this.z]
-            map[this.x][this.y][this.z] = temp;
+            map[tempx][tempy][tempz] = temp;
+            map[tempx+1][tempy][tempz].x -= 1;
+            map[tempx][tempy][tempz].x += 1;
+            map[tempx][tempy][tempz].update = function(){return;};
+            map[tempx+1][tempy][tempz].update = waterupdate;
             return;
         }
     }
@@ -99,6 +113,10 @@ var waterupdate = function(){
             var temp = map[this.x-1][this.y][this.z]
             map[this.x-1][this.y][this.z] = map[this.x][this.y][this.z]
             map[this.x][this.y][this.z] = temp;
+            map[tempx-1][tempy][tempz].x += 1;
+            map[tempx][tempy][tempz].x -= 1;
+            map[tempx][tempy][tempz].update = function(){return;};
+            map[tempx-1][tempy][tempz].update = waterupdate;
             return;
         }
     }
@@ -107,6 +125,10 @@ var waterupdate = function(){
             var temp = map[this.x][this.y+1][this.z]
             map[this.x][this.y+1][this.z] = map[this.x][this.y][this.z]
             map[this.x][this.y][this.z] = temp;
+            map[tempx][tempy+1][tempz].y -= 1;
+            map[tempx][tempy][tempz].y += 1;
+            map[tempx][tempy][tempz].update = function(){return;};
+            map[tempx][tempy+1][tempz].update = waterupdate;
             return;
         }
     }
@@ -115,6 +137,10 @@ var waterupdate = function(){
             var temp = map[this.x][this.y-1][this.z]
             map[this.x][this.y-1][this.z] = map[this.x][this.y][this.z]
             map[this.x][this.y][this.z] = temp;
+            map[tempx][tempy-1][tempz].y += 1;
+            map[tempx][tempy][tempz].y -= 1;
+            map[tempx][tempy][tempz].update = function(){return;};
+            map[tempx][tempy-1][tempz].update = waterupdate;
             return;
         }
     }
@@ -232,12 +258,15 @@ function generateMap(width,height,depth){
     var cavewidth = 5;
     var caveVector = [0,0,0];
     for(var i = 0; i < settings.caveCount; i++){
-        caveVector[2] = (Math.random())+1;
+        caveVector[2] = (Math.random())-0.25;
         caveVector[1] = Math.random()-0.5;
         caveVector[0] = Math.random()-0.5;
         cavex = Math.round(Math.random()*(settings.width-2))+1;
         cavey = Math.round(Math.random()*(settings.height-2))+1
-        cavez = Math.round(gen[cavex][cavey]);
+        if(Math.random() > 0.25)
+            cavez = Math.round(gen[cavex][cavey]);
+        else
+            cavez = Math.round(gen[cavex][cavey]+(Math.random()*gen[cavex][cavey]));
         cavewidth = Math.round((Math.random()*5)+1);
         while(Math.random() > 0.01){
             var z = Math.round(cavez);
@@ -257,6 +286,46 @@ function generateMap(width,height,depth){
             caveVector[1] += Math.random()-0.5;
             caveVector[0] += Math.random()-0.5;
             caveVector[2] += Math.random()-0.5;
+            //cavewidth += Math.round((Math.random()-0.5)*2);
+        }
+    }
+
+    for(k = 0; k < settings.meteors; k++){
+        orex = Math.round(Math.random()*(settings.width-2))+1;
+        orey = Math.round(Math.random()*(settings.height-2))+1
+        orez = Math.round(gen[orex][orey]);
+        oresize = Math.round((Math.random()*10)+1);
+        for(var x = Math.round(orex)-(oresize*2);x < Math.round(orex)+(oresize*2); x++){
+            for(var y = Math.round(orey)-(oresize*2);y < Math.round(orey)+(oresize*2); y++){
+                for(var z = Math.round(orez)-(oresize*2);z < Math.round(orez)+(oresize*2); z++){
+                    if(!soob(x,y,z) && (dist(orex,orey,orez,x,y,z) < oresize)){
+                        if((dist(orex,orey,orez+(oresize*0.75),x,y,z) < oresize*0.5)){
+                            m[x][y][z].texture = "meteorite";
+                        }else{
+                            m[x][y][z].texture = null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for(k = 0; k < settings.nitrogenDeposits; k++){
+        orex = Math.round(Math.random()*(settings.width-2))+1;
+        orey = Math.round(Math.random()*(settings.height-2))+1
+        orez = Math.round(gen[orex][orey]+(Math.random()*settings.groundLevel));
+        oresize = Math.round((Math.random()*10)+1);
+        for(var x = Math.round(orex)-(oresize*2);x < Math.round(orex)+(oresize*2); x++){
+            for(var y = Math.round(orey)-(oresize*2);y < Math.round(orey)+(oresize*2); y++){
+                for(var z = Math.round(orez)-(oresize*2);z < Math.round(orez)+(oresize*2); z++){
+                    if(!soob(x,y,z) && (dist(orex,orey,orez,x,y,z) < oresize)){
+                        if((dist(orex,orey,orez,x,y,z) < oresize) && m[x][y][z].texture != null){
+                            m[x][y][z].texture = "solid-nitrogen";
+                            m[x][y][z].stopDraw = true;
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -288,7 +357,7 @@ function drawAll(){
                 xmargin = 0;
                 ynudge = 0;
                 ymargin = 0;
-                wallThickness = 2;
+                wallThickness = Math.round(settings.tileSize/4);
                 if(z == cameraDepth){
                     map[x][y][z].draw();
                     if(!oob(x+1,y)){
